eb52e4750ad71a75f7d03cbad9ba7ef13156d36e
Luxxn12 <lukman.bip2023@gmail.com>
2025-04-18 20:45:02 +0700

    feat : service microwave

--- app/layout.tsx
+++ app/layout.tsx
@@ -6,18 +6,23 @@ import { ThemeProvider } from "@/components/theme-provider"
 
 const inter = Inter({ subsets: ["latin"] })
 
-// export const metadata: Metadata = {
-//   title: "ServiceMicrowave - Service Microwave Panggilan Cepat & Terpercaya",
-//   description: "Service Microwave Denpasar Layanan perbaikan microwave profesional dengan teknisi berpengalaman di seluruh Bali.",
-// }
 export const metadata: Metadata = {
-  title: "Service Microwave Denpasar - Panggilan Cepat & Bergaransi",
+  title: "SERVICE MICROWAVE DENPASAR - Panggilan Cepat & Bergaransi",
   description: "Service Microwave Denpasar | Perbaikan cepat, bergaransi, dan profesional untuk seluruh wilayah Bali.",
   keywords: ["service microwave denpasar", "perbaikan microwave denpasar", "jasa service microwave bali"],
   robots: "index, follow",
   alternates: {
     canonical: "https://www.servicemicrowavedenpasar.com/",
   },
+  icons: {
+    icon: "/logo.png",
+  },
+  authors: [
+    {
+      name: "Service Microwave Denpasar",
+      url: "https://www.servicemicrowavedenpasar.com/",
+    }
+  ]
 }
 
 
@@ -28,9 +33,6 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="id" suppressHydrationWarning>
-       <head>
-        <link rel="icon" type="image/png" href="/logo.png" />
-      </head>
       <body className={inter.className}>
         <ThemeProvider attribute="class" defaultTheme="light" enableSystem>
           {children}
